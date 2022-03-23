ee9fc0b758770844e38edd9df5b5cab803ffb597
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-03-23 15:20:29 +0100

    sorting ans top 5

---
+++ js/showHighScore.js
@@ -0,0 +1,9 @@
+// import { onClickFunction } from "./onClickFunction.js";
+// import * as checkMatchedBlocks from "./checkMatchedBlocks.js";
+
+// //-----------------Showing the high score ---------------------
+// export function showHighScore(par) {
+//   checkMatchedBlocks.userDataItems.map((m) => {
+//     console.log(`${m.name} - ${m.score}`);
+//   });
+// }
